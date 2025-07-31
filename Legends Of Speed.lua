65810380968201423f95bcf32c11024765eeef51
Vanegood-sus <vanegood2@gmail.com>
2025-07-31 20:01:12 +0300

    Update Legends Of Speed.lua

--- Legends Of Speed.lua
+++ Legends Of Speed.lua
@@ -1,21 +1,16 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostDuckyy/Ui-Librarys/main/Gerad's/source.lua"))()
 
-local window = library:AddWindow("Legend Of Speed", {
+local Window = Library:CreateWindow('vanegood hub ') -- :CreateWindow(Title)
 
-main_color = Color3.fromRGB(200, 200, 200), -- светло-серый
-    min_size = Vector2.new(500, 400),
-    can_resize = true
-})
+local Section = Window:Section('Open script') -- :Section(Title)
 
--- Основная вкладка 
-local farmPlusTab = window:AddTab("Автошаги")
-mainTab:Show()
+-- Label
+Section:Label('Made by vanegood') -- :Label(Text)
 
-farmPlusTab:AddFolder('AutoSteps', function(bool)
+-- Button
+Section:Button('AutoSteps', function()
      getgenv().AutoSteps = true
     while getgenv().AutoSteps == true do
-
-mainTab:AddLabel("Добро Пожаловать!")
 
     local args = {
     [1] = "collectOrb",
@@ -28,13 +23,33 @@ mainTab:AddLabel("Добро Пожаловать!")
   end)
 
 
-mainTab('Автогемы', function()
+Section:Button('AutoGems', function()
      getgenv().AutoGems = true
     while getgenv().AutoGems == true do
       local args = {
       [1] = "collectOrb",
       [2] = "Gem",
       [3] = "City"
+    }
+    game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+  wait()
+    end
+  end)
+
+
+
+Section:Button('AutoRebirth', function()
+     getgenv().AutoRebirth = true
+    while getgenv().AutoRebirth == true do
+     local args = {
+    [1] = "rebirthRequest"
+   }
+game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer(unpack(args))
+    wait()
+    end
+  end)
+
+Section:Label('') -- :Label(Text)      [3] = "City"
     }
     game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
   wait()
