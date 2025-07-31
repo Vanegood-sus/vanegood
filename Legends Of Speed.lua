5bbb19b212a2803700013f998e9cc25606c36f79
Vanegood-sus <vanegood2@gmail.com>
2025-07-31 19:58:03 +0300

    Update Legends Of Speed.lua

--- Legends Of Speed.lua
+++ Legends Of Speed.lua
@@ -1,40 +1,67 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/memejames/elerium-v2-ui-library/main/Library", true))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostDuckyy/Ui-Librarys/main/Gerad's/source.lua"))()
 
--- Создание главного окна со светло-серым цветом
-local window = library:AddWindow("Vanegood Hub", {
-    main_color = Color3.fromRGB(200, 200, 200), -- светло-серый
+local window = library:AddWindow("Legend Of Speed", {
+
+main_color = Color3.fromRGB(200, 200, 200), -- светло-серый
     min_size = Vector2.new(500, 400),
     can_resize = true
 })
 
 -- Основная вкладка 
-local mainTab = window:AddTab("Меню")
+local farmPlusTab = window:AddTab("Автошаги")
 mainTab:Show()
--- AutoSteps
-mainTab:AddButton("AutoSteps", function()
-    getgenv().AutoSteps = true
-    while getgenv().AutoSteps do
-        local args = {
-            [1] = "collectOrb",
-            [2] = "Orange Orb",
-            [3] = "City"
-        }
-        game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
-        task.wait()
-    end
-end)
 
--- AutoGems
-mainTab:AddButton("AutoGems", function()
-    getgenv().AutoGems = true
-    while getgenv().AutoGems do
-        local args = {
-            [1] = "collectOrb",
-            [2] = "Gem",
-            [3] = "City"
-        }
-        game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
-        task.wait()
+farmPlusTab:AddFolder('AutoSteps', function(bool)
+     getgenv().AutoSteps = true
+    while getgenv().AutoSteps == true do
+
+mainTab:AddLabel("Добро Пожаловать!")
+
+    local args = {
+    [1] = "collectOrb",
+    [2] = "Orange Orb",
+    [3] = "City"
+  }
+  game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+  wait()
+    end
+  end)
+
+
+mainTab('Автогемы', function()
+     getgenv().AutoGems = true
+    while getgenv().AutoGems == true do
+      local args = {
+      [1] = "collectOrb",
+      [2] = "Gem",
+      [3] = "City"
+    }
+    game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+  wait()
+    end
+  end)
+
+
+
+local teleportTab = window:AddTab('AutoRebirth', function()
+     getgenv().AutoRebirth = true
+    while getgenv().AutoRebirth == true do
+     local args = {
+    [1] = "rebirthRequest"
+   }
+game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer(unpack(args))
+    wait()
+    end
+  end)
+
+Section:Label('') -- :Label(Text)
+
+
+-- Вкладка "Создатель"
+local noteTab = window:AddTab("Создатель")
+noteTab:AddLabel("Private Script")
+noteTab:AddLabel("")
+noteTab:AddLabel("Созданно vanegood")        task.wait()
     end
 end)
 
